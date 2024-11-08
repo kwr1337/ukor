02ba9f78d5921601c38634036694fbee122fb944
kwr1337 <89042002+kwr1337@users.noreply.github.com>
2024-11-08 04:42:56 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
-    module.exports = {
-    async headers() {
-      return [
-        {
-          source: '/(.*)',
-          headers: [
-            {
-              key: 'Access-Control-Allow-Origin',
-              value: '*', // Разрешает доступ со всех источников
-            },
-          ],
-        },
-      ];
-    },
-  };
-};
-
 export default nextConfig;
